e687faea309c8c89efc1c55243a38b28c8160651
Mumin Khan <mumin.m.khan@gmail.com>
2020-09-04 01:04:00 -0700

    dockerized

---
+++ week03/hw/src/client/opencv.Dockerfile
@@ -0,0 +1,17 @@
+FROM nvcr.io/nvidia/l4t-ml:r32.4.3-py3
+
+RUN apt update && \
+    apt install -y \
+            libffi-dev \
+            python3-pip \
+            python3-tk \ 
+            libopencv-dev \
+            python3-opencv \
+            python3-numpy
+
+#RUN apt install -y python3-scipy python3-matplotlib python3-numpy
+
+COPY test.py test.py
+COPY haarcascade_frontalface_default.xml haarcascade_frontalface_default.xml
+
+CMD ["python3", "-u", "test.py"]
--- week03/hw/src/client/test.py
+++ week03/hw/src/client/test.py
@@ -5,18 +5,21 @@ from datetime import datetime
 from time import sleep
 
 if __name__ == "__main__":
+    print("Begining video stream.")
     video_capture = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
     while True:
         ret, frame = video_capture.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 8)
 
         for (x, y, w, h) in faces:
             cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cur_time = datetime.now().strftime("%m_%d_%Y-%H_%M_%S")
-            cv2.imwrite(f'{cur_time}.png', gray)
+            out_file = f'{cur_time}.png'
+            cv2.imwrite(out_file, gray)
+            print(f'Face found! Written to {out_file}')
             sleep(2)
 
         """
